9627fcfe78f17aca62876dbf74d5c4b40ba066d3
Qharny <cannycc1020@gmail.com>
2024-07-16 10:19:05 +0000

    added new file

--- hello.asm
+++ hello.asm
@@ -1,14 +1,14 @@
-.model small
-.stack 100h
-.data
-message db "hello world$"
-.code
+.model small                ; com program
+.stack 100h                 ; code start here
+.data                       ; define data segment
+message db "hello world$"   ; message to be displayed terminating 
+.code                       ; code segmemt
     Begin:
-    mov dx,OFFSET message
-    mov ax,SEG message
-    mov ds,ax
-    mov ah,9d
-    int 21h
+    mov dx,OFFSET message   ; offset og message terminating with $
+    mov ax,SEG message      ; 
+    mov ds,ax               
+    mov ah,9d               ; function to terminate
+    int 21h                 
     mov ah,4CH
     int 21h
     END Begin
